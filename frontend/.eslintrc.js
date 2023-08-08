500095e4f17f14ffc0b687751bdab546c4e0848a
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-08-08 21:33:59 +0900

    chore: eslint rules/jsx 구문 지원 추가

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,19 +1,21 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:jsx-ally/recommended',
-        'plugin:prettier/recommended',
-        'plugin:react-hooks/recommended',
-    ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: ['react'],
-    rules: {},
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'react-app',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
 };
