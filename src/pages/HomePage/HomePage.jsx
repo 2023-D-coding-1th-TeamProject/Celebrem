b6a9568c406c2afb636c267315be295e18dab0e0
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-08-15 00:17:00 +0900

    feat: 캐러셀 공통 UI 컴포넌트 연결

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 // import styled from 'styled-components';
 import Header from '../../components/common/Header/Header';
+import Carousel from '../../components/common/Carousel/Carousel';
 
 const HomePage = () => {
   return (
     <>
       <Header />
+      <Carousel />
     </>
   );
 };
