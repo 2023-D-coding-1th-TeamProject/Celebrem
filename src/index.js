104aef7edbbc50729d6d421cbcbc30bba621c5e6
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-08-21 15:58:51 +0900

    feat: mock 서버 활성화 코드 추가 (#15)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// 개발 환경에서만 활성화
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
