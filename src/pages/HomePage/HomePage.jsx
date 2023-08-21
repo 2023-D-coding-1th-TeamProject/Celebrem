b153f910e8fd74193a51c8c52e07d40704df62f4
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-08-21 16:06:26 +0900

    feat: 메인 태그 추가 및 인플루언서 태그 필터링 기능 구현 (#15)

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
-// import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
 import Header from '../../components/common/Header/Header';
 import Carousel from '../../components/common/Carousel/Carousel';
+import MainTags from '../../components/common/Tags/MainTags';
+import InfluencerList from '../../components/UserList/InfluencerList';
 
 const HomePage = () => {
+  const [userData, setUserData] = useState([]);
+  const [selectedTag, setSelectedTag] = useState('전체');
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/api/users');
+      const data = await response.json();
+      setUserData(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 인플루언서 태그 필터링
+  const filteredUserData =
+    selectedTag === '전체'
+      ? userData
+      : userData.filter(user => user.category.includes(selectedTag));
+
   return (
     <>
       <Header />
       <Carousel />
+      <MainTags selectedTag={selectedTag} onSelectTag={setSelectedTag} />
+      <InfluencerList userList={filteredUserData} />
     </>
   );
 };
