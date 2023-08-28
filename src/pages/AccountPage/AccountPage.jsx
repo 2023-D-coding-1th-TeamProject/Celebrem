c846e31d5cb730f9f899ac98c9dee0e2a4a80d05
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-08-28 17:23:44 +0900

    feat: 회원 정보 조회 api 연결

--- src/pages/AccountPage/AccountPage.jsx
+++ src/pages/AccountPage/AccountPage.jsx
@@ -1,30 +1,61 @@
 import './AccountPageStyle.jsx';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../components/common/Header/Header';
 import Navbar from '../../components/common/Navbar/Navbar';
-// import Button from '../../components/common/Button/Button';
 import ContentsLayout from '../../layout/ContentsLayout';
-import { MyInfo, H1, Label, Email } from './AccountPageStyle.jsx';
+import { MyInfo, H1, Label, Description, Email, Empty } from './AccountPageStyle.jsx';
+import { getMyProfile } from '../../apis/user.js';
+import Spinner from '../../components/common/Spinner/Spinner.jsx';
+// import ProfileTags from '../../components/common/Tags/ProfileTags.jsx';
 
 const AccountPage = () => {
   const navigate = useNavigate();
-  return (
-    <>
-      <Header />
-      <ContentsLayout>
-        <Navbar />
-        <MyInfo>
-          <H1>회원정보</H1>
-          <Label>이메일</Label>
-          <Email>celebrem@test.com</Email>
-          <Label>소개</Label>
-          <p>등록된 소개글이 없습니다.</p>
-          <button onClick={() => navigate('edit')}>등록하러 가기</button>
-        </MyInfo>
-      </ContentsLayout>
-    </>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [userProfile, setUserProfile] = useState(null);
+
+  useEffect(() => {
+    const handleProfile = async () => {
+      const profileData = await getMyProfile();
+      setUserProfile(profileData);
+      setIsLoading(false);
+      console.log(profileData);
+    };
+    handleProfile();
+  }, []);
+
+  if (isLoading) {
+    return <Spinner />;
+  } else {
+    return (
+      <>
+        <Header />
+        <ContentsLayout>
+          <Navbar profile={userProfile} />
+          <MyInfo>
+            <H1>회원정보</H1>
+            <Label>이메일</Label>
+            <Email>{userProfile?.email}</Email>
+            <Label>소개</Label>
+            {userProfile?.description ? (
+              <>
+                <Description placeholder="나에 대한 소개글을 작성해주세요!">
+                  {userProfile?.description}
+                </Description>
+                <button onClick={() => navigate('edit')}>수정하기</button>
+              </>
+            ) : (
+              <>
+                <Empty>등록된 소개 글이 없습니다.</Empty>
+                <button onClick={() => navigate('edit')}>등록하러 가기</button>
+              </>
+            )}
+            {/* <ProfileTags profile={userProfile} /> */}
+          </MyInfo>
+        </ContentsLayout>
+      </>
+    );
+  }
 };
 
 export default AccountPage;
