8717f71285fbf71f7e6c03b1763b82c9b2725918
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-08-30 21:18:57 +0900

    feat: 액세스 토큰 만료 시, 리프레쉬 토큰 재발급 구현

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { getCookie } from './cookie';
+import { getCookie, setCookie, removeCookie } from './cookie';
+import { postRefreshToken } from './auth';
 
 const URL = process.env.REACT_APP_API_URL;
 
@@ -10,27 +11,62 @@ export const instance = axios.create({
   },
 });
 
+export const imgInstance = axios.create({
+  baseURL: URL,
+  headers: {
+    'Content-Type': 'multipart/form-data',
+  },
+});
+
 export const authInstance = axios.create({
   baseURL: URL,
   headers: {
-    Authorization: `Bearer ${getCookie('token')}`,
     'Content-Type': 'application/json',
   },
 });
 
 authInstance.interceptors.request.use(
   config => {
-    const token = getCookie('token');
-    if (!config.headers.Authorization) {
-      config.headers = {
-        ...config.headers,
-        Authorization: `Bearer ${token}`,
-      };
-    }
+    const accessToken = getCookie('accessToken');
+    config.headers.Authorization = `Bearer ${accessToken}`; // 수정된 부분
     return config;
   },
   error => {
-    console.log(error.message);
     return Promise.reject(error);
   },
 );
+
+authInstance.interceptors.response.use(
+  async function (response) {
+    return response;
+  },
+  async function (error) {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const accessToken = getCookie('accessToken');
+        const refreshToken = getCookie('refreshToken');
+        const refreshResponse = await postRefreshToken(accessToken, refreshToken);
+        console.log(refreshResponse);
+        const newAccessToken = refreshResponse.accessToken;
+        const newRefreshToken = refreshResponse.refreshToken;
+        setCookie('accessToken', newAccessToken);
+        setCookie('refreshToken', newRefreshToken);
+
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return await authInstance.request(originalRequest);
+      } catch (error) {
+        removeCookie('accessToken');
+        removeCookie('refreshToken');
+        alert('로그인이 만료되었습니다.');
+
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default authInstance;
