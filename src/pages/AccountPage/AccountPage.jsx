d5d7a0f14816f5095ceba489d91802b3bb91d9b0
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-09-01 22:29:51 +0900

    feat: 소개글 유무에 따른 버튼 변경

--- src/pages/AccountPage/AccountPage.jsx
+++ src/pages/AccountPage/AccountPage.jsx
@@ -1,18 +1,22 @@
 import './AccountPageStyle.jsx';
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 import Header from '../../components/common/Header/Header';
 import Navbar from '../../components/common/Navbar/Navbar';
+import Button from '../../components/common/Button/Button.jsx';
 import ContentsLayout from '../../layout/ContentsLayout';
 import { MyInfo, H1, Label, Description, Email, Empty } from './AccountPageStyle.jsx';
 import { getMyProfile } from '../../apis/user.js';
 import Spinner from '../../components/common/Spinner/Spinner.jsx';
-// import ProfileTags from '../../components/common/Tags/ProfileTags.jsx';
+import { roleState } from '../../atoms/userAtom.jsx';
+import ProfileTags from '../../components/common/Tags/ProfileTags.jsx';
 
 const AccountPage = () => {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(true);
   const [userProfile, setUserProfile] = useState(null);
+  const userRole = useRecoilValue(roleState);
 
   useEffect(() => {
     const handleProfile = async () => {
@@ -42,15 +46,24 @@ const AccountPage = () => {
                 <Description placeholder="나에 대한 소개글을 작성해주세요!">
                   {userProfile?.description}
                 </Description>
-                <button onClick={() => navigate('edit')}>수정하기</button>
+                {userRole === 'ROLE_INFLUENCER' && (
+                  <ProfileTags categories={userProfile.profileTagNames} />
+                )}
+                <Button size="md" variant="soft" onClick={() => navigate('/mypage/edit')}>
+                  수정하기
+                </Button>
               </>
             ) : (
               <>
                 <Empty>등록된 소개 글이 없습니다.</Empty>
-                <button onClick={() => navigate('edit')}>등록하러 가기</button>
+                {userRole === 'ROLE_INFLUENCER' && (
+                  <ProfileTags categories={userProfile.profileTagNames} />
+                )}
+                <Button size="md" variant="soft" onClick={() => navigate('/mypage/edit')}>
+                  등록하러 가기
+                </Button>
               </>
             )}
-            {/* <ProfileTags profile={userProfile} /> */}
           </MyInfo>
         </ContentsLayout>
       </>
