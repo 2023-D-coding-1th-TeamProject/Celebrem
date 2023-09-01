e76e28dd95b2a8e226fde093e0fe986f33a43596
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-09-01 22:32:21 +0900

    feat: 회원가입, 액세스/리프레쉬 토큰 api 추가

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -3,11 +3,18 @@ import { authInstance } from './axiosInstance';
 
 /* 로그인 */
 export const userLogin = async (email, password) => {
-  const response = await instance.post('/auth/login', {
-    email: email,
-    password: password,
-  });
-  return response.data;
+  try {
+    const response = await instance.post('/auth/login', {
+      email: email,
+      password: password,
+    });
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    if (error.response.status === 401) {
+      alert('이메일 혹은 비밀번호가 일치하지 않습니다.');
+    }
+  }
 };
 
 /* 회원가입 */
@@ -46,10 +53,10 @@ export const emailVerificationSend = async email => {
 };
 
 /* 리프레시 토큰으로 만료된 액세스 토큰 재발급 */
-export const postRefreshToken = async () => {
-  const refreshToken = localStorage.getItem('refreshToken');
+export const postRefreshToken = async (accessToken, refreshToken) => {
   const response = await authInstance.post(`/auth/reissue`, {
     refreshToken: refreshToken,
+    accessToken: accessToken,
   });
   return response.data;
 };
