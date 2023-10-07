bbac478c4a86c87be2fbe818804bdaff08144f8a
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-10-07 23:01:41 +0900

    chore: React Query 설정 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import GlobalReset from './styles/GlobalReset';
 import { ThemeProvider } from 'styled-components';
 import Theme from './styles/Theme';
 import AppRouter from './routes/AppRouter';
 
 const App = () => {
+  const queryClient = new QueryClient();
   return (
     <>
       <GlobalReset />
-      <ThemeProvider theme={Theme}>
-        <AppRouter />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={Theme}>
+          <AppRouter />
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 };
